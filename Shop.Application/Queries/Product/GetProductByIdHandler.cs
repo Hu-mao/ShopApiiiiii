ca6493e881c6c355c05f778130caaf430eae8c55
XXXX <plantsvs233zmbie@gmail.com>
2026-09-09 12:04:36 +0300

    IDN

---
+++ Shop.Application/Queries/Product/GetProductByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Shop.Application.DTOs.CategoryDTOs;
+using Shop.Application.DTOs.ProductDTOs;
+using Shop.Application.Interfaces.Repository;
+
+namespace Shop.Application.Queries.GetProductById;
+
+public class GetProductByIdHandler
+    : IRequestHandler<GetProductByIdQuery, ProductReadDTO?>
+{
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public GetProductByIdHandler(
+        IProductRepository productRepository,
+        IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ProductReadDTO?> Handle(
+        GetProductByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.id);
+
+        if (product == null)
+            return null;
+
+        return _mapper.Map<ProductReadDTO>(product);
+    }
+}
---
+++ Shop.Application/Queries/Product/GetProductByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shop.Application.DTOs.ProductDTOs;
+
+namespace Shop.Application.Queries.GetProductById;
+
+public record GetProductByIdQuery(int id) : IRequest<ProductReadDTO?>;
